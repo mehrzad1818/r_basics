3db4df4139f2babca35577835db34523c0e94aef
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-04-02 22:06:58 +0330

    Update 001_data_preparation.r

--- 001_data_preparation.r
+++ 001_data_preparation.r
@@ -5,3 +5,23 @@ Salaries <- read_csv("salaries.csv")
 
 # import data from a tab delimited file
 Salaries <- read_tsv("salaries.txt")
+
+"rank","discipline","yrs.since.phd","yrs.service","sex","salary"
+"Prof","B",19,18,"Male",139750
+"Prof","B",20,16,"Male",173200
+"AsstProf","B",4,3,"Male",79750
+"Prof","B",45,39,"Male",115000
+"Prof","B",40,41,"Male",141500
+"AssocProf","B",6,6,"Male",97000
+
+
+library(haven)
+
+# import data from Stata
+Salaries <- read_dta("salaries.dta")
+
+# import data from SPSS
+Salaries <- read_sav("salaries.sav")
+
+# import data from SAS
+Salaries <- read_sas("salaries.sas7bdat")
