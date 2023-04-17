ca9b259589fb6bd90af65d214505023b10673010
Mehrzad Barzegar <37508338+mehrzad1818@users.noreply.github.com>
2023-04-17 23:59:38 +0330

    Update 001_data_preparation.r

--- 001_data_preparation.r
+++ 001_data_preparation.r
@@ -37,3 +37,12 @@ newdata <- select(starwars, name, mass:species)
 
 # keep all variables except birth_year and gender
 newdata <- select(starwars, -birth_year, -gender)
+
+
+"rank","discipline","yrs.since.phd","yrs.service","sex","salary"
+"Prof","B",19,18,"Male",139750
+"Prof","B",20,16,"Male",173200
+"AsstProf","B",4,3,"Male",79750
+"Prof","B",45,39,"Male",115000
+"Prof","B",40,41,"Male",141500
+"AssocProf","B",6,6,"Male",97000
